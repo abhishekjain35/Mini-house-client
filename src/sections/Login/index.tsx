87de80ad0ef81855cbb9cfca03ccf2db8418d285
Abhishek Jain <abhishekjai35@gmail.com>
2021-04-11 14:10:53 +0530

    Added login mutation and loading

--- src/sections/Login/index.tsx
+++ src/sections/Login/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import { useApolloClient } from "@apollo/react-hooks";
-import { AUTH_URL } from "../../lib/graphql/queries/AuthUrl";
-import { AuthUrl } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
-import { Card, Layout, Typography } from "antd";
+import React, { useEffect, useRef } from "react";
+import { Card, Layout, Spin, Typography } from "antd";
 import googleLogo from "./assets/google_logo.jpg";
+import { LOG_IN } from "../../lib/graphql/mutations";
+import { AUTH_URL } from "../../lib/graphql/queries";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from "../../lib/graphql/mutations/logIn/__generated__/LogIn";
 import { Viewer } from "../../lib/types";
 const { Content } = Layout;
 const { Text, Title } = Typography;
+
 interface Props {
   setViewer: (viewer: Viewer) => void;
 }
@@ -14,14 +20,42 @@ interface Props {
 export const Login = ({ setViewer }: Props) => {
   let client = useApolloClient();
 
+  const [
+    logIn,
+    { data: logInData, loading: logInLoading, error: logInError },
+  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+      }
+    },
+  });
+
+  const loginRef = useRef(logIn);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    if (code) {
+      loginRef.current({
+        variables: {
+          input: { code },
+        },
+      });
+    }
+  }, []);
+
   const handleAuthorize = async () => {
     try {
-      const { data } = await client.query<AuthUrl>({
+      const { data } = await client.query<AuthUrlData>({
         query: AUTH_URL,
       });
       window.location.href = data.authUrl;
     } catch {}
   };
+
+  if(logInLoading){
+    return <Content><Spin size="large" tip="Logging you in..." /></Content>
+  }
   return (
     <Content className="log-in">
       <Card className="log-in-card">
