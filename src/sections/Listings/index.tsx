9a59a4fd69b0c8735bd8f50bc5a8d09b39f166df
Abhishek Jain <abhishekjai35@gmail.com>
2021-05-06 13:41:09 +0530

    Added SearchResults and location based rendering of listings component

--- src/sections/Listings/index.tsx
+++ src/sections/Listings/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
-import { Layout, List } from "antd";
+import { Layout, List, Typography } from "antd";
 import { ListingCard } from "../../lib/components";
 import { LISTINGS } from "../../lib/graphql/queries";
 import {
@@ -8,13 +8,20 @@ import {
   ListingsVariables,
 } from "../../lib/graphql/queries/Listings/__generated__/Listings";
 import { ListingsFilter } from "../../lib/graphql/globalTypes";
+import { Link, RouteComponentProps } from "react-router-dom";
+
+interface MatchParams {
+  location: string;
+}
 
 const { Content } = Layout;
+const { Title, Paragraph, Text } = Typography;
 const PAGE_LIMIT = 8;
 
-export const Listings = () => {
+export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
   const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
     variables: {
+      location: match.params.location,
       filter: ListingsFilter.PRICE_LOW_TO_HIGH,
       limit: PAGE_LIMIT,
       page: 1,
@@ -22,23 +29,47 @@ export const Listings = () => {
   });
 
   const listings = data ? data.listings : null;
+  const listingsRegion = listings ? listings.region : null;
 
-  const listingsSectionElement = listings ? (
-    <List
-      grid={{
-        gutter: 8,
-        xs: 1,
-        sm: 2,
-        lg: 4,
-      }}
-      dataSource={listings.result}
-      renderItem={(listing) => (
-        <List.Item>
-          <ListingCard listing={listing} />
-        </List.Item>
-      )}
-    />
+  const listingsSectionElement =
+    listings && listings.result.length ? (
+      <List
+        grid={{
+          gutter: 8,
+          xs: 1,
+          sm: 2,
+          lg: 4,
+        }}
+        dataSource={listings.result}
+        renderItem={(listing) => (
+          <List.Item>
+            <ListingCard listing={listing} />
+          </List.Item>
+        )}
+      />
+    ) : (
+      <div>
+        <Paragraph>
+          It appears that no listings have yet been created for{" "}
+          <Text mark>"{listingsRegion}"</Text>
+        </Paragraph>
+        <Paragraph>
+          Be the first person to create a{" "}
+          <Link to="/host">listing in this area</Link>!
+        </Paragraph>
+      </div>
+    );
+
+  const listingsRegionElement = listingsRegion ? (
+    <Title level={3} className="listings__title">
+      Results for "{listingsRegion}"
+    </Title>
   ) : null;
 
-  return <Content className="listings">{listingsSectionElement}</Content>;
+  return (
+    <Content className="listings">
+      {listingsRegionElement}
+      {listingsSectionElement}
+    </Content>
+  );
 };
