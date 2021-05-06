8748f22039ecf41200ef733d44916c448b4c1427
Abhishek Jain <abhishekjai35@gmail.com>
2021-05-06 20:48:43 +0530

    Added ListingsPagination and ListingsSkeleton components

--- src/sections/Listings/index.tsx
+++ src/sections/Listings/index.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
-import { Layout, List, Typography } from "antd";
-import { ListingCard } from "../../lib/components";
-import { ListingsFilters } from "./components";
+import { Layout, List, Typography, Affix } from "antd";
+import { ErrorBanner, ListingCard } from "../../lib/components";
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from "./components";
 import { LISTINGS } from "../../lib/graphql/queries";
 import {
   Listings as ListingsData,
@@ -21,14 +25,35 @@ const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
   const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
-    variables: {
-      location: match.params.location,
-      filter,
-      limit: PAGE_LIMIT,
-      page: 1,
-    },
-  });
+  const [page, setPage] = useState(1);
+
+  const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        filter,
+        limit: PAGE_LIMIT,
+        page,
+      },
+    }
+  );
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner description="We either couldn't find anything matching your search or have encountered an error. If you're searching for a unique location, try searching again with more common keywords." />
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
 
   const listings = data ? data.listings : null;
   const listingsRegion = listings ? listings.region : null;
@@ -36,7 +61,15 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
   const listingsSectionElement =
     listings && listings.result.length ? (
       <div>
-        <ListingsFilters filter={filter} setFilter={setFilter} />
+        <Affix offsetTop={64}>
+          <ListingsPagination
+            total={listings.total}
+            page={page}
+            limit={PAGE_LIMIT}
+            setPage={setPage}
+          />
+          <ListingsFilters filter={filter} setFilter={setFilter} />
+        </Affix>
         <List
           grid={{
             gutter: 8,
