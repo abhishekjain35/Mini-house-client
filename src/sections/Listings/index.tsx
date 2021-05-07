68b0500cd68decda4196f76ceefe9430eb0e2101
Abhishek Jain <abhishekjai35@gmail.com>
2021-05-07 15:13:16 +0530

    Added SearchBar in AppHeader

--- src/sections/Listings/index.tsx
+++ src/sections/Listings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { Layout, List, Typography, Affix } from "antd";
 import { ErrorBanner, ListingCard } from "../../lib/components";
@@ -24,12 +24,14 @@ const { Title, Paragraph, Text } = Typography;
 const PAGE_LIMIT = 8;
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const locationRef = useRef(match.params.location);
   const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
   const [page, setPage] = useState(1);
 
   const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
     LISTINGS,
     {
+      skip: locationRef.current !== match.params.location && page !== 1,
       variables: {
         location: match.params.location,
         filter,
@@ -38,6 +40,11 @@ export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
       },
     }
   );
+
+  useEffect(() => {
+    setPage(1);
+    locationRef.current = match.params.location;
+  }, [match.params.location]);
 
   if (loading) {
     return (
